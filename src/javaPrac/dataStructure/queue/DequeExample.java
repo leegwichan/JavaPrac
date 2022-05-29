e380d236dff45c74c91576fabb1515eea712097c
leegwichan <cnddkscndgus@naver.com>
2022-05-29 21:05:01 +0900

    [자료구조] 디큐(Deque) 사용해보기 - 1

---
+++ src/javaPrac/dataStructure/queue/DequeExample.java
@@ -0,0 +1,10 @@
+package javaPrac.dataStructure.queue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class DequeExample {
+    public static void main(String[] args) {
+        Deque<String> deque = new LinkedList<>();
+    }
+}
